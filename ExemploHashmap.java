201ab457377932fe9b5bf220f137df46def85ee8
Manoella Waideman <mherreriaswaideman@gmail.com>
2024-08-13 11:10:28 -0300

    Create ExemploHashmap.java

---
+++ ExemploHashmap.java
@@ -0,0 +1,36 @@
+package br.com.fiap.main;
+
+import javax.swing.*;
+import java.util.HashMap;
+
+public class ExemploHashMap {
+    public static void main(String[] args) {
+        HashMap<String, String> mapa = new HashMap<String, String>();
+        do {
+            try {
+                String sigla, estado;
+                do {
+                    sigla = JOptionPane.showInputDialog("Digite a UF de um estado ou digite \"FIM\"").toUpperCase();
+                    if (!sigla.equals("FIM")){
+                        estado = JOptionPane.showInputDialog("Digite o nome complesto do estado informado anteriormente");
+                        if (mapa.containsKey(sigla)){
+                            JOptionPane.showMessageDialog(null, "Estado já foi cadastrado.");
+                        } else {
+                            mapa.put(sigla, estado);
+                        }
+                    }
+                } while (!sigla.equals("FIM"));
+                String escolha = JOptionPane.showInputDialog("Digite a UF de um Estado a sua escolha.").toUpperCase();
+                if (mapa.containsKey(escolha)){
+                    JOptionPane.showMessageDialog(null, "O nome completo deste Estado é: " + mapa.get(escolha));
+                }else {
+                    JOptionPane.showMessageDialog(null, "Estado ainda não cadastrado.");
+                }
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, e.getMessage());
+            }
+
+        } while(JOptionPane.showConfirmDialog(null, "Deseja continuar?", "Atenção", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE) == 0);
+        JOptionPane.showMessageDialog(null, "Fim do programa!");
+    }
+}
